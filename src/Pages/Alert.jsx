45f925d5fa25af270fa2618603c9b0fdf63a381f
kamilov1701 <azizbekkamilov1701@gmail.com>
2026-02-21 10:13:13 +0500

    Deployment to vercel

--- src/Pages/Alert.jsx
+++ src/Pages/Alert.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Alert() {
-    return <div className="p-4 text-center font-bold text-xl">Alert Page</div>;
+    return <div>Alert Page</div>;
 }
 
 export default Alert;
