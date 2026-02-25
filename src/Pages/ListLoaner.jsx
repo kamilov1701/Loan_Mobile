8dad58a0d2a9849d8a8afd43092a3d1cb60be9f3
sobirjon <sobirjonotjonov11@gmail.com>
2026-02-25 21:41:31 +0500

    loan list end

--- src/Pages/ListLoaner.jsx
+++ src/Pages/ListLoaner.jsx
@@ -1,7 +1,253 @@
 import React from "react";
+// Images{
+import plus from "../../public/plus.png"
+import minus from "../../public/minus.png"
+import time from "../../public/Clock.png"
+import eye from "../../public/Eye.png"
+import pen from "../../public/pen.png"
+import del from "../../public/delete.png"
+// }
+// Icons{
+import { ArrowLeft } from 'lucide-react';
+// }
 
 function ListLoaner() {
-  return <div className="p-4 text-center font-bold text-xl">List Loaner Page</div>;
+  return <div>
+
+    <nav>
+      <div className="flex items-center gap-[51px] p-[27px] bg-[#FFFFFF] border boder-[#E5E7EB]">
+        <ArrowLeft />
+        <h2 className="text-center font-bold text-xl">Qarzdorlar Ro’yxati</h2>
+      </div>
+    </nav>
+    <section className="px-[20px]">
+
+      <section>
+
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#ffffff] border-[#E5E7EB] border px-[9px] py-[8px] rounded-[12px] mt-[14px]">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-[9px]">
+              <div>
+                <span className="bg-[#DBEAFE] text-[11px] font-bold text-[#197FE6] px-[8px] py-[9px] rounded-full">SO</span>
+              </div>
+
+              <div>
+                <h2 className="text-[#000000] text-[14px] font-bold">Sobirjon Otajanov</h2>
+                <p className="text-[#64748B] text-[11px] font-normal">Comment</p>
+              </div>
+            </div>
+
+            <div>
+              <h2 className="text-[#197FE6] text-[14px] font-bold ml-[50px] mb-[15px">150.000 so’m</h2>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center ml-[24px]">
+            <div className="flex w-[150px] gap-[17px]">
+              <img src={plus} alt="" />
+              <img src={minus} alt="" />
+              <img src={time} alt="" />
+            </div>
+            <div className="bg-[#FFF7ED] py-[9px] px-[12px] rounded-full w-[150px] flex justify-between">
+              <img src={eye} alt="" />
+              <img src={pen} alt="" />
+              <img src={del} alt="" />
+            </div>
+          </div>
+        </div>
+
+
+      </section>
+
+    </section>
+
+  </div>;
 }
 
 export default ListLoaner;
